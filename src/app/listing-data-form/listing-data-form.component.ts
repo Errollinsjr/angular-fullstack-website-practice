0255206f4697d82616cbccbbbf08d61240ec66de
Rollins, Edward <errollins.jr@gmail.com>
2022-05-13 02:24:11 -0400

    use eventEmitter for buttons and input and output annotations

--- src/app/listing-data-form/listing-data-form.component.ts
+++ src/app/listing-data-form/listing-data-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
+import { Listing } from '../types';
 
 @Component({
   selector: 'app-listing-data-form',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listing-data-form.component.css']
 })
 export class ListingDataFormComponent implements OnInit {
+  @Input() buttonText;
+  name: string = '';
+  description: string = '';
+  price: string = '';
 
-  constructor() { }
+  @Output() onSubmit = new EventEmitter<Listing>();
+
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onButtonClicked(): void {
+    // alert('Creating a new listing. . .');
+    // this.router.navigateByUrl('/my-listings');
+    this.onSubmit.emit({
+      id: null || " ",
+      name: this.name,
+      description: this.description,
+      price: Number(this.price),
+    });
   }
 
 }
