d89b5c568ad8e94f9140246980292770af80a818
Rollins, Edward <errollins.jr@gmail.com>
2022-05-13 02:48:35 -0400

    finish nav bar and edit listing page fill

--- src/app/edit-listing-page/edit-listing-page.component.ts
+++ src/app/edit-listing-page/edit-listing-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Listing } from '../types';
+import { fakeListings } from '../fake-data';
 
 @Component({
   selector: 'app-edit-listing-page',
@@ -7,12 +9,16 @@ import { Router } from '@angular/router';
   styleUrls: ['./edit-listing-page.component.css']
 })
 export class EditListingPageComponent implements OnInit {
+  listing!: Listing;
 
   constructor( 
+    private route: ActivatedRoute,
     private router: Router, 
   ) {}
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.listing = fakeListings.find(listing => listing.id === id) || {};
   }
 
   onSubmit(): void {
