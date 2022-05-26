b9a775df3137d5a4b268ead265fda29e369fbeb8
Rollins, Edward <errollins.jr@gmail.com>
2022-05-26 14:50:38 -0400

    finish up deleting listings via service and onClick in my listings p

--- src/app/my-listings-page/my-listings-page.component.ts
+++ src/app/my-listings-page/my-listings-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { fakeListings } from '../fake-data';
+import { ListingsService } from '../listings.service';
 import { Listing } from '../types';
 
 @Component({
@@ -10,14 +10,19 @@ import { Listing } from '../types';
 export class MyListingsPageComponent implements OnInit {
   listings: Listing[] = [];
 
-  constructor() { }
+  constructor(
+    private listingsService: ListingsService
+  ) { }
 
   ngOnInit(): void {
-    this.listings = fakeListings;
+    this.listingsService.getListingsForUser().subscribe(listings => this.listings = listings);
   }
 
-  onDeletedClicked(listingId?: string): void {
+  onDeletedClicked(listingId: string): void {
     alert(`Deleting your listing with id ${listingId}`);
+    this.listingsService.deleteListing(listingId).subscribe(() => {
+      this.listings = this.listings.filter(listing => listing.id !== listingId);
+    })
   }
 
 }
